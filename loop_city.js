fc64c3ad52d3632867ea41c9e84dc282ae903f36
Braden Amorozo <braden808@gmail.com>
2016-11-28 08:43:58 -1000

    update

---
+++ loop_city.js
@@ -0,0 +1,7 @@
+for (var count = 0; count <= 100; count++) {
+	console.log("I can count to "count);
+}
+
+for (var animalCount = 0; animalCount <= 5; count++) {
+	console.log("there are " + animalCount + "many animals in Noahs Arc")
+}
